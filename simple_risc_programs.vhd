4526952c13a1bba67509132bb5d5b3b333043973
Esme Genereux <esme.genereux@polymtl.ca>
2021-03-27 14:26:29 -0400

    exo 10 complete

--- simple_risc_programs.vhd
+++ simple_risc_programs.vhd
@@ -24,6 +24,26 @@ package simple_risc_programs is
    );
 
    constant program_1 : inst_mem_type := (
+      -- init
+      x"A000001", -- 1                             - addr x00                          
+      x"A010000", -- c                             - addr x01
+      x"A020000", -- p                             - addr x02 
+      x"A030000", -- t                             - addr x03 
+      x"A040006", -- n                             - addr x04 
+      x"A050000", -- ptr                           - addr x05 
+
+      -- do while (n != 0)
+      x"7030100", -- r[t]        <- r[c]           - addr x06 
+      x"0010001", -- r[c]        <- 1 + r[c]       - addr x07 
+      x"0010201", -- r[c]        <- r[p] + r[c]    - addr x08 
+      x"7020300", -- r[p]        <- r[t]           - addr x09 
+      x"9000501", -- Md[r[ptr]]  <- r[c]           - addr x0A 
+      x"0050005", -- r[ptr]      <- 1 + r[ptr]     - addr x0B 
+      x"1040400", -- r[n]        <- r[n] - 1       - addr x0C
+
+      -- jump if (n != 0)
+      x"C020006",
+
       others => (others => '1')
    );
    
